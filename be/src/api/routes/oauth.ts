7d7368626b70710fa32858418e76b98b6e123ac8
yunho <kimyouknow@naver.com>
2022-06-29 09:49:55 +0900

    [BE] Feat: oauth이후 token 쿠키에 담아서 전달

--- be/src/api/routes/oauth.ts
+++ be/src/api/routes/oauth.ts
@@ -18,7 +18,7 @@ export default (app: Router) => {
     res.json({ url: `https://github.com/login/oauth/authorize?client_id=${clientId}` });
   });
 
-  route.get('/github', (req, res) => {
+  route.get('/github/callback', (req, res) => {
     const body = {
       client_id: clientId,
       client_secret: clientSecret,
@@ -30,25 +30,28 @@ export default (app: Router) => {
       .then((res) => res.data['access_token'])
       .then((token) => {
         console.log('My token:', token);
+        res.cookie('token-redirect', token, { httpOnly: true });
         res.redirect('http://localhost:3000/');
       })
       .catch((err) => res.status(500).json({ message: err.message }));
   });
 
-  route.post('/github/callback', (req, res) => {
+  route.post('/github/callback', async (req, res) => {
     const body = {
       client_id: cliendIdWithCallback,
       client_secret: cliendSecretWithCallback,
       code: req.body.code,
     };
     const opts = { headers: { accept: 'application/json' } };
-    axios
-      .post(`https://github.com/login/oauth/access_token`, body, opts)
-      .then((res) => res.data['access_token'])
-      .then((token) => {
-        console.log('My token:', token);
-        res.json({ token });
-      })
-      .catch((err) => res.status(500).json({ message: err.message }));
+    try {
+      const response = await axios.post(`https://github.com/login/oauth/access_token`, body, opts);
+      const token = response.data['access_token'];
+      console.log('My token:', token);
+      res.cookie('token-callback', token, { httpOnly: true });
+      res.json({ token });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: error });
+    }
   });
 };
--- be/src/loaders/express.ts
+++ be/src/loaders/express.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import routes from '../api';
+import config from '../config';
 
 const corsOptions = {
   origin: 'http://localhost:3000',
@@ -12,5 +13,5 @@ export default ({ app }: { app: express.Application }) => {
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
-  app.use('/api', routes());
+  app.use(config.API.PREFIX, routes());
 };
